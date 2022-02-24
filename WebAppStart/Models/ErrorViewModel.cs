19e8a6ac3f62ad27b09db5d000e2ffde9d19da86
Wrong <ilak71363@gmail.com>
2022-02-24 19:07:15 +0600

    Добавьте файлы проекта.

---
+++ WebAppStart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppStart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
