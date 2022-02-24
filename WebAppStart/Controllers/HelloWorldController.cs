faaed045b6d847ee8dc8c050250ddb281cc15d6e
Wrong <ilak71363@gmail.com>
2022-02-24 19:29:19 +0600

    Контроллер

---
+++ WebAppStart/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace WebAppStart.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // HelloWorld/
+       public string Index()
+        {
+            return "Это стоковое действие";
+        }
+
+        // HelloWorld/Welcome
+        public string Welcome(string name,int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
