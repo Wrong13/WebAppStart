e45e5e05f4fe791f283c682a0a6c3811c9939b23
Wrong <ilak71363@gmail.com>
2022-02-27 15:06:40 +0600

    Немного аннотации

--- WebAppStart/Models/Movie.cs
+++ WebAppStart/Models/Movie.cs
@@ -5,11 +5,14 @@ namespace WebAppStart.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Display(Name = "Название")]
         public string? Title { get; set; }
-
+        [Display(Name = "Дата выхода")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name ="Жанр")]
         public string? Genre { get; set; }
+        [Display(Name ="Цена")]
         public decimal Price { get; set; }
     }
 }
