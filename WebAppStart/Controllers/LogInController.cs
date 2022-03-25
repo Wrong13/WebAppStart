e091d9626c240a471ddea2a355d4adf8915255f9
Wrong <ilak71363@gmail.com>
2022-03-25 20:40:09 +0600

    ковыляю

---
+++ WebAppStart/Controllers/LogInController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppStart.Models;
+
+namespace WebAppStart.Controllers
+{
+    public class LogInController : Controller
+    {
+        WebAppStart.Data.WebAppStartContext context;
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateUser(User AddUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                context.User.Add(AddUser);
+                context.SaveChanges();
+            }
+            return View(Index());
+        }
+
+    }
+}
--- WebAppStart/Models/User.cs
+++ WebAppStart/Models/User.cs
@@ -2,13 +2,9 @@
 {
     public class User
     {
-        public User()
-        {
-            this.Baskets = new HashSet<Basket>();
-        }
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public ICollection<Basket> Baskets { get; set; }
+        public List<Basket> Baskets { get; set; }
     }
 }
